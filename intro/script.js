77a1acdf978797dbd2ecd23bed818c2752cb99aa
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2026-03-22 23:51:18 +0530

    Update script.js

--- intro/script.js
+++ intro/script.js
@@ -1,97 +1,149 @@
 const express = require("express")
-const jwt = require("jsonwebtoken")
-const {UserModel, TodoModel} = require("./db")
 const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const { UserModel, MyCoursesModel, CreateCourseModel } = require("./db")
 
-const JWT_SECRET = "ayushmaan"
+mongoose.connect("mongodb+srv://ayushmaanshukla274:Ayush24@cluster0.2lqbam1.mongodb.net/myApp")
 
-mongoose.connect("mongodb+srv://ayushmaanshukla274:Ayush24@cluster0.2lqbam1.mongodb.net/todo-app")
+const JWT_SECRET = "courseApp"
 
 const app = express()
+
 app.use(express.json())
+app.use(express.static(__dirname + "/public"))
 
-app.post("/signup", async function(req, res){
-    const email = req.body.email
-    const password = req.body.password
-    const name = req.body.name
-
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    })
-
-    res.json({
-        message: "You are logged in."
-    })
+app.get("/coursehub.com", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html")
 })
 
-app.post("/signin", async function(req, res){
-    const email = req.body.email
-    const password = req.body.password
+// Signup Endpoint
+app.post("/coursehub.com/signup", async function (req, res) {
 
-    const user = await UserModel.findOne({
-        email: email,
-        password: password
-    })
+    try {
+        const email = req.body.email
+        const username = req.body.username
+        const password = req.body.password
 
-    if(user){
-        const token = jwt.sign({
-            id: user._id.toString()
-        }, JWT_SECRET)
-        res.json({
-            message: "Signned In!",
-            token: token
+        const existingUser = await UserModel.findOne({ email })
+        if (existingUser) {
+            return res.json("User Already Exist.")
+        }
+
+        await UserModel.create({
+            email: email,
+            username: username,
+            password: password
         })
-    } else {
+
         res.json({
-            message: "Incorrect credentials."
+            message: "Signed up! User added to database."
         })
+
+    } catch (error) {
+        res.json("Server Error")
     }
+
 })
 
-auth = (req, res, next) => {
-    const token = req.headers.token
-    const decodedInformation = jwt.verify(token, JWT_SECRET)
 
-    if(decodedInformation){
-        req.userId = decodedInformation.id
-        next()
-    } else {
+// Signin Endpoint
+app.post("/coursehub.com/signin", async function (req, res) {
+    const email = req.body.email
+    const password = req.body.password
+
+    try {
+        const user = await UserModel.findOne({
+            email: email,
+            password: password
+        })
+
+        if (user) {
+            const token = jwt.sign({
+                id: user._id.toString()
+            }, JWT_SECRET)
+
+            res.json({
+                token: token,
+                message: "Signed In"
+            })
+        } else {
+            return res.status(403).json({
+                message: "Invalid credentials"
+            })
+        }
+    } catch (error) {
         res.status(403).json({
             message: "Incorrect Credentials"
         })
     }
-}
-
-//create todo
-app.post("/todo", auth, function(req, res){
-    const userId = req.userId
-    const title = req.body.title
-
-    TodoModel.create({
-        title,
-        userId
-    })
-
-    res.json({
-        userId: userId,
-        message: "Todo Created!"
-    })
 })
 
-//get the existing todos from the database
-app.get("/todos", auth, async function(req, res){
-    const userId = req.userId
-    const title = req.body.title
 
-    const todos = await TodoModel.find({
-        userId: userId
+// Authentication
+const auth = (req, res, next) => {
+    const token = req.headers.token;
+    try {
+        const decodedInformation = jwt.verify(token, JWT_SECRET);
+        req.userId = decodedInformation.id;
+        next();
+    } catch (err) {
+        res.status(401).json({
+            message: "Unauthorized Request!"
+        });
+    }
+};
+
+
+//My Courses
+app.get("/coursehub.com/mycourses", auth, async function (req, res) {
+    const courses = await MyCoursesModel.findOne({
+        userId: req.userId
     })
 
-    res.json({
-        todos: todos
-    })
+    if (!courses) {
+        return res.json({
+            message: "You do not purchased any course yet."
+        })
+    } else {
+        res.json({
+            message: "You courses:",
+            courses: courses
+        })
+    }
 })
 
-app.listen(3000)
+// Purchase a course
+app.post("/coursehub.com/purchasecourse", auth, async function (req, res) {
+    const courseName = req.body.courseName
+    const instructor = req.body.instructor
+    const done = req.body.done
+
+    await MyCoursesModel.create({
+        courseName: courseName,
+        instructor: instructor,
+        done: done,
+        userId: req.userId
+    })
+
+    res.json("You've purchased this course.")
+})
+
+
+// Create a course
+app.post("/coursehub.com/createCourse", async function (req, res) {
+    const courseName = req.body.courseName
+    const instructor = req.body.instructor
+    const duration = req.body.duration
+    const price = req.body.price
+
+    const course = await CreateCourseModel.create({
+        courseName: courseName,
+        instructor: instructor,
+        duration: duration,
+        price: price
+    })
+
+    res.json("Course Added!")
+})
+
+app.listen(4000)
