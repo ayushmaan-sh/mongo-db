1c0300f9cbc6c4d8f4db301fd6a2b45de15aaffd
ayushmaan-sh <ayushmaanshukla274@gmail.com>
2026-06-28 19:19:44 +0530

    Added signin route & signed with JWT

--- script.js
+++ script.js
@@ -40,5 +40,34 @@ app.post("/signup", async(req, res)=>{
     }
 })
 
+app.post("/signin", async(req, res)=>{
+    const username = req.body.username
+    const password = req.body.password
+
+    try {       
+        const user = UserModel.findOne({
+            username: username,
+            password: password
+        })
+    
+        if(user){
+            const token = jwt.sign({username}, JWT_SECRET)   
+            res.json({
+                message: "Signed In",
+                token: token
+            })
+        } else {
+            message: "Invalid Credentials."
+        }
+
+    } catch (error) {
+        res.json({
+            error: error,
+            message: "Something went wrong."
+        })
+    }
+
+})
+
 
 app.listen(3000)
